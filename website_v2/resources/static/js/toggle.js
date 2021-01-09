587a45f096271ba943bbd726205a5039de2bfa50
yacine-yr <73900639+yacine-yr@users.noreply.github.com>
2021-01-09 17:46:06 +0100

    mostly changing ids and adding text areas for the main 4 indexes from folder Cozy room V1, also adding toggle.js for toggling the 8 sub indexes

---
+++ website_v2/resources/static/js/toggle.js
@@ -0,0 +1,61 @@
+
+	function hideValues() {
+			var x = document.getElementById("coltime");
+			if (x.style.display === "none") {
+			x.style.display = "block";
+			} else {
+			x.style.display = "none";
+			 }
+			
+			var x = document.getElementById("coltemp");
+			if (x.style.display === "none") {
+			x.style.display = "block";
+			} else {
+			x.style.display = "none";
+			 }
+			 var x = document.getElementById("collight");
+			if (x.style.display === "none") {
+			x.style.display = "block";
+			} else {
+			x.style.display = "none";
+			 }
+			var x = document.getElementById("colsound");
+			if (x.style.display === "none") {
+			x.style.display = "block";
+			} else {
+			x.style.display = "none";
+			 }
+			
+			
+			var x = document.getElementById("colhumid");
+			if (x.style.display === "none") {
+			x.style.display = "block";
+			} else {
+			x.style.display = "none";
+			 }
+			
+			
+			var x = document.getElementById("colco2");
+			if (x.style.display === "none") {
+			x.style.display = "block";
+			} else {
+			x.style.display = "none";
+			 }
+			
+			
+			var x = document.getElementById("colpm10");
+			if (x.style.display === "none") {
+			x.style.display = "block";
+			} else {
+			x.style.display = "none";
+			 }
+			
+			
+			var x = document.getElementById("colno2");
+			if (x.style.display === "none") {
+			x.style.display = "block";
+			} else {
+			x.style.display = "none";
+			 }
+			
+		}
